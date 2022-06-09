c26a13454326cde20a3602336c394fc260d37fe1
Pablo Velasquez <pabloavelasquez@gmail.com>
2022-06-08 21:26:09 -0500

    Informes semestre 2022-1

---
+++ 20221/nuevos-informes-semestre-matriculaciones-inscripciones-2021-2.sql
@@ -0,0 +1,190 @@
+/* Nuevos informes semestre Matriculaciones - Inscripciones 2022-1 */
+SELECT (@cnt := @cnt + 1) AS Id, "2021" as "AÑO","II" as "PERIODO/SEMESTRE", REPLACE(ccc.name,"Facultad de ","") as "FACULTAD/DEPENDENCIA", (SELECT cat.name FROM mdl_course_categories cat where cat.id = ccc.parent) Nivel,
+
+IF(ccc.name = "Facultad de Derecho"
+    OR ccc.name = "Departamento de Idiomas - FDE"
+    OR ccc.name = "Departamento de Matemáticas",
+(SELECT COUNT(distinct u.id) as "Inscripciones Estudiantes Derecho"
+FROM mdl_course c
+INNER JOIN mdl_context ctx ON ctx.instanceid = c.id
+INNER JOIN mdl_role_assignments ra ON ctx.id = ra.contextid
+INNER JOIN mdl_role r ON r.id = ra.roleid
+INNER JOIN mdl_user u ON u.id = ra.userid
+INNER JOIN mdl_course_categories cc on c.category = cc.id
+inner join mdl_enrol e on e.courseid =c.id
+INNER JOIN mdl_user_enrolments ue on ue.userid = u.id and ue.enrolid = e.id
+WHERE r.shortname = "student"
+
+AND
+(
+    (
+        ccc.parent = 2 
+        AND 
+        REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 2 /* PREGRADO */
+        AND 
+        (
+            DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= "2021-01-01" AND DATE_FORMAT(FROM_UNIXTIME(ue.timeend), '%Y-%m-%d' ) <= "2022-01-30" /* PREGRADO CAL A */
+            OR 
+            DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= "2021-06-16" AND DATE_FORMAT(FROM_UNIXTIME(ue.timeend), '%Y-%m-%d' ) <= "2022-07-31" /* PREGRADO CAL B */
+        )
+        AND
+        (
+            REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", '') != 339 /* Diferente Departamento de Matemáticas */
+            OR
+            (
+                REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", '') = 339 /* Departamento de Matemáticas */
+                AND
+                INSTR(lower(c.fullname),"derecho") != 0 /* Aula de la Facultad de Derecho */
+            )
+        )
+    )
+    OR
+    (
+        ccc.parent = 2 
+        AND 
+        REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 2 /* PREGRADO */
+        AND 
+        DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= "2021-06-16" 
+        AND
+        REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", '') = 339 /* Departamento de Matemáticas */
+        AND
+        INSTR(lower(c.fullname),"derecho") = 0 /* Aula que no es de la Facultad de Derecho */
+    )
+    OR
+    (
+        ccc.parent = 6 
+        AND 
+        REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 6 /* POSGRADO */  
+        AND 
+        DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= "2021-06-16" 
+    )
+    OR
+    (
+        ccc.parent = 11 
+        AND 
+        REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 11 /* EC */
+        AND 
+        DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= "2021-06-16" 
+    )
+)
+
+AND (select cccc.name from mdl_course_categories cccc where cccc.id
+ = (REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", ''))) = ccc.name
+Order BY c.id asc
+),
+(SELECT COUNT(distinct u.id) as "Inscripciones Estudiantes"
+FROM mdl_course c
+INNER JOIN mdl_context ctx ON ctx.instanceid = c.id
+INNER JOIN mdl_role_assignments ra ON ctx.id = ra.contextid
+INNER JOIN mdl_role r ON r.id = ra.roleid
+INNER JOIN mdl_user u ON u.id = ra.userid
+INNER JOIN mdl_course_categories cc on c.category = cc.id
+inner join mdl_enrol e on e.courseid =c.id
+INNER JOIN mdl_user_enrolments ue on ue.userid = u.id and ue.enrolid = e.id
+WHERE r.shortname = "student"
+AND
+DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= "2021-06-16"
+AND REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = ccc.parent
+  
+AND (select cccc.name from mdl_course_categories cccc where cccc.id = (REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", ''))) = ccc.name
+Order BY c.id asc
+)
+) as "Estudiantes",
+
+
+IF(ccc.name = "Facultad de Derecho"
+    OR ccc.name = "Departamento de Idiomas - FDE"
+    OR ccc.name = "Departamento de Matemáticas",
+(SELECT COUNT(u.id) as "Inscripciones Estudiantes Derecho"
+FROM mdl_course c
+INNER JOIN mdl_context ctx ON ctx.instanceid = c.id
+INNER JOIN mdl_role_assignments ra ON ctx.id = ra.contextid
+INNER JOIN mdl_role r ON r.id = ra.roleid
+INNER JOIN mdl_user u ON u.id = ra.userid
+INNER JOIN mdl_course_categories cc on c.category = cc.id
+inner join mdl_enrol e on e.courseid =c.id
+INNER JOIN mdl_user_enrolments ue on ue.userid = u.id and ue.enrolid = e.id
+WHERE r.shortname = "student"
+
+AND
+(
+    (
+        ccc.parent = 2 
+        AND 
+        REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 2 /* PREGRADO */
+        AND 
+        (
+            DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= "2021-01-01" AND DATE_FORMAT(FROM_UNIXTIME(ue.timeend), '%Y-%m-%d' ) <= "2022-01-30" /* PREGRADO CAL A */
+            OR 
+            DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= "2021-06-16" AND DATE_FORMAT(FROM_UNIXTIME(ue.timeend), '%Y-%m-%d' ) <= "2022-07-31" /* PREGRADO CAL B */
+        )
+        AND
+        (
+            REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", '') != 339 /* Diferente Departamento de Matemáticas */
+            OR
+            (
+                REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", '') = 339 /* Departamento de Matemáticas */
+                AND
+                INSTR(lower(c.fullname),"derecho") != 0 /* Aula de la Facultad de Derecho */
+            )
+        )
+    )
+    OR
+    (
+        ccc.parent = 2 
+        AND 
+        REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 2 /* PREGRADO */
+        AND 
+        DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= "2021-06-16" 
+        AND
+        REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", '') = 339 /* Departamento de Matemáticas */
+        AND
+        INSTR(lower(c.fullname),"derecho") = 0 /* Aula que no es de la Facultad de Derecho */
+    )
+    OR
+    (
+        ccc.parent = 6 
+        AND 
+        REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 6 /* POSGRADO */  
+        AND 
+        DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= "2021-06-16" 
+    )
+    OR
+    (
+        ccc.parent = 11 
+        AND 
+        REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 11 /* EC */
+        AND 
+        DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= "2021-06-16" 
+    )
+)
+
+AND (select cccc.name from mdl_course_categories cccc where cccc.id
+ = (REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", ''))) = ccc.name
+Order BY c.id asc
+),
+(SELECT COUNT(u.id) as "Inscripciones Estudiantes"
+FROM mdl_course c
+INNER JOIN mdl_context ctx ON ctx.instanceid = c.id
+INNER JOIN mdl_role_assignments ra ON ctx.id = ra.contextid
+INNER JOIN mdl_role r ON r.id = ra.roleid
+INNER JOIN mdl_user u ON u.id = ra.userid
+INNER JOIN mdl_course_categories cc on c.category = cc.id
+inner join mdl_enrol e on e.courseid =c.id
+INNER JOIN mdl_user_enrolments ue on ue.userid = u.id and ue.enrolid = e.id
+WHERE r.shortname = "student"
+AND
+DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= "2021-06-16"
+AND REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = ccc.parent
+  
+AND (select cccc.name from mdl_course_categories cccc where cccc.id = (REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", ''))) = ccc.name
+Order BY c.id asc
+)
+) as "Incripciones"
+
+FROM mdl_course_categories ccc
+CROSS JOIN (SELECT @cnt := 0) AS dummy
+WHERE (ccc.parent = 2 /* PREGRADO */
+OR ccc.parent = 6 /* POSGRADO */
+OR ccc.parent = 11) /* EC */
+ORDER BY (SELECT @cnt := 0),ccc.name ASC
