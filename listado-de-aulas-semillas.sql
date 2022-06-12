7185c899cdd32d30868391fdca3b5a79ff3131f7
Pablo Velasquez <pabloavelasquez@gmail.com>
2022-06-11 19:58:55 -0500

    Informe aulas semillas

--- listado-de-aulas-semillas.sql
+++ listado-de-aulas-semillas.sql
@@ -1,75 +1,90 @@
-SELECT c.id Id_curso, c.fullname Curso, c.shortname NombreCorto,
+SELECT c.id AS "Id", c.fullname  AS "Aula semilla", c.shortname AS "Nombre corto", c.timecreated AS "Fecha de creación",
 CASE
-    WHEN LOCATE ("-v-i-", c.shortname) THEN "100% Virtual"
-    WHEN LOCATE ("-v-I-", c.shortname) THEN "100% Virtual"
-    WHEN LOCATE ("-V-i-", c.shortname) THEN "100% Virtual"
-    WHEN LOCATE ("-V-I-", c.shortname) THEN "100% Virtual"
-    WHEN LOCATE ("-m-i-", c.shortname) THEN "Blended"
-    WHEN LOCATE ("-m-I-", c.shortname) THEN "Blended"
-    WHEN LOCATE ("-M-i-", c.shortname) THEN "Blended"
-    WHEN LOCATE ("-M-I-", c.shortname) THEN "Blended"
+    WHEN LOCATE ("-v-i-", LOWER(c.shortname)) THEN "100% Virtual"
+    WHEN LOCATE ("-m-i-", LOWER(c.shortname)) THEN "Blended"
     ELSE "Aula apoyo"
 END "Tipo aula nombre corto",
 
- (SELECT REPLACE(JSON_EXTRACT(CAST(CONCAT('["',REPLACE(REPLACE(JSON_EXTRACT(cff.configdata, '$.options'),'"',''),'\\r\\n','","'),'"]') as JSON), CONCAT('$[',cfd.intvalue-1,']')),'"','') AS "Tipo Aula"
- FROM {context} ctxt
- INNER JOIN {customfield_data} cfd ON cfd.contextid = ctxt.id
- INNER JOIN {customfield_field} cff ON cff.id = cfd.fieldid
-  WHERE ctxt.instanceid = c.id) "Tipo Aula",
+(
+  SELECT REPLACE(JSON_EXTRACT(CAST(CONCAT('["',REPLACE(REPLACE(JSON_EXTRACT(cff.configdata, '$.options'),'"',''),'\\r\\n','","'),'"]') as JSON), CONCAT('$[',cfd.intvalue-1,']')),'"','') AS "Tipo Aula"
+  FROM {context} ctxt
+  INNER JOIN {customfield_data} cfd ON cfd.contextid = ctxt.id
+  INNER JOIN {customfield_field} cff ON cff.id = cfd.fieldid
+  WHERE ctxt.instanceid = c.id
+) AS "Tipo Aula",
 
- c.format Formato,
+ c.format AS "Formato",
 
- (SELECT count(cest.id) estudiantes FROM {course} cest
-INNER JOIN {context} ctxest ON ctxest.instanceid = cest.id
-INNER JOIN {role_assignments} raest ON ctxest.id = raest.contextid
-INNER JOIN {role} rest ON rest.id = raest.roleid
-INNER JOIN {user} uest ON uest.id = raest.userid
-INNER JOIN {course_categories} ccest on cest.category = ccest.id
-inner join {enrol} eest on eest.courseid =cest.id
-INNER JOIN {user_enrolments} ueest on ueest.userid = uest.id and ueest.enrolid = eest.id
-WHERE DATE_FORMAT(FROM_UNIXTIME(ueest.timeend), '%Y-%m-%d' ) >= CURDATE() 
-AND (rest.shortname = "student")
-AND cest.id = c.id
-GROUP BY cest.id
-Order BY cest.id asc) Estudiantes_activos,
+(
+  SELECT IF(count(survey.id)>0,"SI","NO") encuestas FROM {feedback} survey
+  WHERE 
+  LOWER(survey.name) LIKE "%satisfacci%"
+  AND survey.course = c.id
+) AS "Encuesta Satisfacción",
 
- (SELECT count(cest.id) estudiantes FROM {course} cest
-INNER JOIN {context} ctxest ON ctxest.instanceid = cest.id
-INNER JOIN {role_assignments} raest ON ctxest.id = raest.contextid
-INNER JOIN {role} rest ON rest.id = raest.roleid
-WHERE rest.shortname = "student"
-AND cest.id = c.id
-Order BY cest.id asc) Estudiantes,
+(
+  SELECT count(cest.id) estudiantes FROM {course} cest
+  INNER JOIN {context} ctxest ON ctxest.instanceid = cest.id
+  INNER JOIN {role_assignments} raest ON ctxest.id = raest.contextid
+  INNER JOIN {role} rest ON rest.id = raest.roleid
+  INNER JOIN {user} uest ON uest.id = raest.userid
+  INNER JOIN {course_categories} ccest on cest.category = ccest.id
+  inner join {enrol} eest on eest.courseid =cest.id
+  INNER JOIN {user_enrolments} ueest on ueest.userid = uest.id and ueest.enrolid = eest.id
+  WHERE
+  (
+    DATE_FORMAT(FROM_UNIXTIME(ueest.timeend), '%Y-%m-%d' ) >= CURDATE()
+    OR DATE_FORMAT(FROM_UNIXTIME(ueest.timeend), '%Y-%m-%d' ) = "1969-12-31"
+  )
+  AND (rest.shortname = "student")
+  AND cest.id = c.id
+  GROUP BY cest.id
+  Order BY cest.id asc
+) AS "Estudiantes activos hoy",
+
+(
+  SELECT count(cest.id) estudiantes FROM {course} cest
+  INNER JOIN {context} ctxest ON ctxest.instanceid = cest.id
+  INNER JOIN {role_assignments} raest ON ctxest.id = raest.contextid
+  INNER JOIN {role} rest ON rest.id = raest.roleid
+  WHERE rest.shortname = "student"
+  AND cest.id = c.id
+  Order BY cest.id asc
+) AS "Estudiantes",
   
-(SELECT GROUP_CONCAT(CONCAT(upro.firstname, " ",upro.lastname) SEPARATOR ' / ') as Profesor
- FROM {course} cpro
-INNER JOIN {context} ctxpro ON ctxpro.instanceid = cpro.id
-INNER JOIN {role_assignments} rapro ON ctxpro.id = rapro.contextid
-INNER JOIN {role} rpro ON rpro.id = rapro.roleid
-INNER JOIN {user} upro ON upro.id = rapro.userid
-INNER JOIN {course_categories} ccpro on cpro.category = ccpro.id
-inner join {enrol} epro on epro.courseid =cpro.id
-INNER JOIN {user_enrolments} uepro on uepro.userid = upro.id and uepro.enrolid = epro.id
-WHERE (rpro.shortname = "teacher" OR rpro.shortname = "editingteacher")
-AND cpro.id = c.id
-GROUP BY cpro.id
-Order BY cpro.id asc) Profesor,
+(
+  SELECT GROUP_CONCAT(CONCAT(upro.firstname, " ",upro.lastname) SEPARATOR ' / ') as Profesor
+  FROM {course} cpro
+  INNER JOIN {context} ctxpro ON ctxpro.instanceid = cpro.id
+  INNER JOIN {role_assignments} rapro ON ctxpro.id = rapro.contextid
+  INNER JOIN {role} rpro ON rpro.id = rapro.roleid
+  INNER JOIN {user} upro ON upro.id = rapro.userid
+  INNER JOIN {course_categories} ccpro on cpro.category = ccpro.id
+  inner join {enrol} epro on epro.courseid =cpro.id
+  INNER JOIN {user_enrolments} uepro on uepro.userid = upro.id and uepro.enrolid = epro.id
+  WHERE (rpro.shortname = "teacher" OR rpro.shortname = "editingteacher")
+  AND cpro.id = c.id
+  GROUP BY cpro.id
+  Order BY cpro.id asc
+) AS "Profesor",
    
-(SELECT GROUP_CONCAT(CONCAT(upro.email) SEPARATOR ' / ') as Profesor
- FROM {course} cpro
-INNER JOIN {context} ctxpro ON ctxpro.instanceid = cpro.id
-INNER JOIN {role_assignments} rapro ON ctxpro.id = rapro.contextid
-INNER JOIN {role} rpro ON rpro.id = rapro.roleid
-INNER JOIN {user} upro ON upro.id = rapro.userid
-INNER JOIN {course_categories} ccpro on cpro.category = ccpro.id
-inner join {enrol} epro on epro.courseid =cpro.id
-INNER JOIN {user_enrolments} uepro on uepro.userid = upro.id and uepro.enrolid = epro.id
-WHERE (rpro.shortname = "teacher" OR rpro.shortname = "editingteacher")
-AND cpro.id = c.id
-GROUP BY cpro.id
-Order BY cpro.id asc) "Profesor email",
+(
+  SELECT GROUP_CONCAT(CONCAT(upro.email) SEPARATOR ' / ') as Profesor
+  FROM {course} cpro
+  INNER JOIN {context} ctxpro ON ctxpro.instanceid = cpro.id
+  INNER JOIN {role_assignments} rapro ON ctxpro.id = rapro.contextid
+  INNER JOIN {role} rpro ON rpro.id = rapro.roleid
+  INNER JOIN {user} upro ON upro.id = rapro.userid
+  INNER JOIN {course_categories} ccpro on cpro.category = ccpro.id
+  inner join {enrol} epro on epro.courseid =cpro.id
+  INNER JOIN {user_enrolments} uepro on uepro.userid = upro.id and uepro.enrolid = epro.id
+  WHERE (rpro.shortname = "teacher" OR rpro.shortname = "editingteacher")
+  AND cpro.id = c.id
+  GROUP BY cpro.id
+  Order BY cpro.id asc
+) AS "Profesor email",
  
- c.visible CursoVisible,
+ c.visible AS "Visible",
  (select cat.name from {course_categories} cat where cat.id = 
   REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '')) CAT1,
  (select cat.name from {course_categories} cat where cat.id = 
@@ -87,7 +102,7 @@ Order BY cpro.id asc) "Profesor email",
 FROM {course} c
 INNER JOIN {course_categories} cc on c.category = cc.id
 
-WHERE REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 319
+WHERE REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 319 /* SEMILLAS */
 
 GROUP BY c.id
 Order BY c.id asc
