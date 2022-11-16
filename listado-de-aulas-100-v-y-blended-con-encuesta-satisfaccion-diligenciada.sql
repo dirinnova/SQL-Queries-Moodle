bb1da292697941bd7e37138fb0b958446ff344f1
Pablo <pabloavelasquez@gmail.com>
2022-11-16 14:58:18 -0500

    Encuestas de satisfacción

---
+++ listado-de-aulas-100-v-y-blended-con-encuesta-satisfaccion-diligenciada.sql
@@ -0,0 +1,208 @@
+/* Listado de aulas 100% V y Blended con encuesta satisfacción diligenciada
+
+Esta consulta solo devuelve el listado de aulas solo 100% virtual y blended con la fecha fin de los estudiantes y que tienen encuesta de satisfacción diligenciada minimo por un participante
+-- Listado de campos que devuelve la consulta:
+### Id_curso:
+Es el número consecutivo de la tabla {course}
+
+### Curso
+Nombre del curso (aula)
+
+### Nombre corto
+Nombre corto del curso (aula)
+
+### Formato
+Es el formato que tiene configurada el aula actualmente.
+
+### Visible
+Visibilidad del curso (aula)
+
+### Tipo Aula
+Es la configuración que tiene el aula en las opciones general del aula
+*/
+SELECT Id_curso AS "Id", Curso AS "Aula", NombreCorto AS "Nombre corto", Formato, CursoVisible AS "Visible",
+Tipoaula AS "Tipo Aula",/*Tipoaulanombrecorto AS "Tipo aula nombre corto",*/nombreencuesta AS "Encuesta de Satisfacción", TotalPreguntas AS "Preguntas Encuesta", TotalRespuestas AS "Respuestas Encuesta", /*Totalrespuestasdetalle AS "Detalle respuestas",*/Estudiantesactivoshoy AS "Estudiantes activos a hoy",Estudiantes, fechainicio, fechafin,
+Profesor,Profesoremail AS "Profesor email",CAT1,CAT2,CAT3,CAT4,CAT5,CAT6,CAT7, MAX(fin)
+FROM 
+(
+  SELECT ueest.id idenrol,c.id Id_curso, c.fullname Curso, c.shortname NombreCorto, c.format Formato, c.visible CursoVisible,
+  ueest.timestart fechainicio, DATE_FORMAT(FROM_UNIXTIME(ueest.timeend), '%d/%m/%Y' ) fechafin,
+  count(DATE_FORMAT(FROM_UNIXTIME(ueest.timeend), '%d/%m/%Y' )) fin,
+  /*
+  CASE
+    WHEN LOCATE ("-v-i-", LOWER(c.shortname)) THEN "100% Virtual"
+    WHEN LOCATE ("-m-i-", LOWER(c.shortname)) THEN "Blended"
+    ELSE "Aula apoyo"
+  END Tipoaulanombrecorto,
+*/
+  (
+    SELECT REPLACE(JSON_EXTRACT(CAST(CONCAT('["',REPLACE(REPLACE(JSON_EXTRACT(cff.configdata, '$.options'),'"',''),'\\r\\n','","'),'"]') as JSON), CONCAT('$[',cfd.intvalue-1,']')),'"','') AS "Tipo Aula"
+    FROM {context} ctxt
+    INNER JOIN {customfield_data} cfd ON cfd.contextid = ctxt.id
+    INNER JOIN {customfield_field} cff ON cff.id = cfd.fieldid
+    WHERE ctxt.instanceid = c.id
+  ) TipoAula,
+
+  survey.name nombreencuesta,
+
+  (
+    SELECT COUNT(DISTINCT fipreg.id) Preguntas
+    FROM {feedback} fepreg
+    INNER JOIN {feedback_item} fipreg ON fipreg.feedback = fepreg.id
+    INNER JOIN {feedback_value} fvpreg ON fvpreg.item = fipreg.id
+    WHERE fepreg.course = c.id
+    /* fepreg.id = survey.id */
+    AND LOWER(fepreg.name) LIKE "%satisfacci%"
+  ) TotalPreguntas,
+
+  (
+    SELECT COUNT(DISTINCT fvpreg.completed) Respuestas
+    FROM {feedback} fepreg
+    INNER JOIN {feedback_item} fipreg ON fipreg.feedback = fepreg.id
+    INNER JOIN {feedback_value} fvpreg ON fvpreg.item = fipreg.id
+    WHERE fepreg.course = c.id
+    /* fepreg.id = survey.id */
+    AND LOWER(fepreg.name) LIKE "%satisfacci%"
+  ) TotalRespuestas,
+/*
+  (
+    SELECT COUNT(fvpreg.id) Respuestas
+    FROM {feedback} fepreg
+    INNER JOIN {feedback_item} fipreg ON fipreg.feedback = fepreg.id
+    INNER JOIN {feedback_value} fvpreg ON fvpreg.item = fipreg.id
+    WHERE fepreg.course = c.id
+    AND fvpreg.value != "" AND fvpreg.value != " " AND fvpreg.value != "\n"
+    AND LOWER(fepreg.name) LIKE "%satisfacci%"
+  ) Totalrespuestasdetalle,
+*/
+  (
+    SELECT count(cest.id) estudiantes FROM {course} cest
+    INNER JOIN {context} ctxest ON ctxest.instanceid = cest.id
+    INNER JOIN {role_assignments} raest ON ctxest.id = raest.contextid
+    INNER JOIN {role} rest ON rest.id = raest.roleid
+    INNER JOIN {user} uest ON uest.id = raest.userid
+    INNER JOIN {course_categories} ccest on cest.category = ccest.id
+    inner join {enrol} eest on eest.courseid =cest.id
+    INNER JOIN {user_enrolments} ueest on ueest.userid = uest.id and ueest.enrolid = eest.id
+    WHERE
+    (
+      DATE_FORMAT(FROM_UNIXTIME(ueest.timeend), '%Y-%m-%d' ) >= CURDATE()
+      OR DATE_FORMAT(FROM_UNIXTIME(ueest.timeend), '%Y-%m-%d' ) = "1969-12-31"
+    )
+    AND (rest.shortname = "student")
+    AND cest.id = c.id
+    GROUP BY cest.id
+    Order BY cest.id asc
+  ) Estudiantesactivoshoy,
+
+  (
+    SELECT count(cest.id) estudiantes FROM {course} cest
+    INNER JOIN {context} ctxest ON ctxest.instanceid = cest.id
+    INNER JOIN {role_assignments} raest ON ctxest.id = raest.contextid
+    INNER JOIN {role} rest ON rest.id = raest.roleid
+    WHERE rest.shortname = "student"
+    AND cest.id = c.id
+    Order BY cest.id asc
+  ) Estudiantes,
+    
+  (
+    SELECT GROUP_CONCAT(CONCAT(upro.firstname, " ",upro.lastname) SEPARATOR ' / ') as Profesor
+    FROM {course} cpro
+    INNER JOIN {context} ctxpro ON ctxpro.instanceid = cpro.id
+    INNER JOIN {role_assignments} rapro ON ctxpro.id = rapro.contextid
+    INNER JOIN {role} rpro ON rpro.id = rapro.roleid
+    INNER JOIN {user} upro ON upro.id = rapro.userid
+    INNER JOIN {course_categories} ccpro on cpro.category = ccpro.id
+    inner join {enrol} epro on epro.courseid =cpro.id
+    INNER JOIN {user_enrolments} uepro on uepro.userid = upro.id and uepro.enrolid = epro.id
+    WHERE (rpro.shortname = "teacher" OR rpro.shortname = "editingteacher")
+    AND cpro.id = c.id
+    GROUP BY cpro.id
+    Order BY cpro.id asc
+  ) Profesor,
+    
+  (
+    SELECT GROUP_CONCAT(CONCAT(upro.email) SEPARATOR ' / ') as Profesor
+    FROM {course} cpro
+    INNER JOIN {context} ctxpro ON ctxpro.instanceid = cpro.id
+    INNER JOIN {role_assignments} rapro ON ctxpro.id = rapro.contextid
+    INNER JOIN {role} rpro ON rpro.id = rapro.roleid
+    INNER JOIN {user} upro ON upro.id = rapro.userid
+    INNER JOIN {course_categories} ccpro on cpro.category = ccpro.id
+    inner join {enrol} epro on epro.courseid =cpro.id
+    INNER JOIN {user_enrolments} uepro on uepro.userid = upro.id and uepro.enrolid = epro.id
+    WHERE (rpro.shortname = "teacher" OR rpro.shortname = "editingteacher")
+    AND cpro.id = c.id
+    GROUP BY cpro.id
+    Order BY cpro.id asc
+  ) Profesoremail,
+
+  (select cat.name from {course_categories} cat where cat.id = 
+    REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '')) CAT1,
+  (select cat.name from {course_categories} cat where cat.id = 
+    REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", '')) CAT2,
+  (select cat.name from {course_categories} cat where cat.id = 
+    REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 4),LENGTH(SUBSTRING_INDEX(cc.path, "/", 4-1)) + 1),"/", '')) CAT3,
+  (select cat.name from {course_categories} cat where cat.id = 
+    REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 5),LENGTH(SUBSTRING_INDEX(cc.path, "/", 5-1)) + 1),"/", '')) CAT4,
+  (select cat.name from {course_categories} cat where cat.id = 
+    REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 6),LENGTH(SUBSTRING_INDEX(cc.path, "/", 6-1)) + 1),"/", '')) CAT5,
+  (select cat.name from {course_categories} cat where cat.id = 
+    REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 7),LENGTH(SUBSTRING_INDEX(cc.path, "/", 7-1)) + 1),"/", '')) CAT6,
+  (select cat.name from {course_categories} cat where cat.id = 
+    REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 8),LENGTH(SUBSTRING_INDEX(cc.path, "/", 8-1)) + 1),"/", '')) CAT7
+  FROM {course} c
+
+  INNER JOIN {context} ctxest ON ctxest.instanceid = c.id
+  INNER JOIN {role_assignments} raest ON ctxest.id = raest.contextid
+  INNER JOIN {role} rest ON rest.id = raest.roleid
+  INNER JOIN {user} uest ON uest.id = raest.userid
+  INNER JOIN {course_categories} cc on c.category = cc.id
+  INNER JOIN {enrol} eest on eest.courseid = c.id
+  INNER JOIN {user_enrolments} ueest on ueest.userid = uest.id and ueest.enrolid = eest.id
+  INNER JOIN {feedback} survey on survey.course = c.id
+  INNER JOIN {feedback_completed} surveycomp on surveycomp.feedback = survey.id
+
+  WHERE (REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 2
+  OR REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 6
+    OR REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 11)
+
+  AND 
+  (
+    LOWER(c.shortname) LIKE "%-v-i-%"
+    OR 
+    LOWER(c.shortname) LIKE "%-m-i-%" 
+    OR 
+    LOWER(c.shortname) LIKE "%-v-e-%"
+    OR
+    (
+      SELECT REPLACE(JSON_EXTRACT(CAST(CONCAT('["',REPLACE(REPLACE(JSON_EXTRACT(cff.configdata, '$.options'),'"',''),'\\r\\n','","'),'"]') as JSON), CONCAT('$[',cfd.intvalue-1,']')),'"','') AS "Tipo Aula"
+      FROM {context} ctxt
+      INNER JOIN {customfield_data} cfd ON cfd.contextid = ctxt.id
+      INNER JOIN {customfield_field} cff ON cff.id = cfd.fieldid
+      WHERE ctxt.instanceid = c.id
+    ) = "100% Virtual"
+    OR
+    (
+      SELECT REPLACE(JSON_EXTRACT(CAST(CONCAT('["',REPLACE(REPLACE(JSON_EXTRACT(cff.configdata, '$.options'),'"',''),'\\r\\n','","'),'"]') as JSON), CONCAT('$[',cfd.intvalue-1,']')),'"','') AS "Tipo Aula"
+      FROM {context} ctxt
+      INNER JOIN {customfield_data} cfd ON cfd.contextid = ctxt.id
+      INNER JOIN {customfield_field} cff ON cff.id = cfd.fieldid
+      WHERE ctxt.instanceid = c.id
+    ) = "Blended"
+    OR
+    (
+      SELECT REPLACE(JSON_EXTRACT(CAST(CONCAT('["',REPLACE(REPLACE(JSON_EXTRACT(cff.configdata, '$.options'),'"',''),'\\r\\n','","'),'"]') as JSON), CONCAT('$[',cfd.intvalue-1,']')),'"','') AS "Tipo Aula"
+      FROM {context} ctxt
+      INNER JOIN {customfield_data} cfd ON cfd.contextid = ctxt.id
+      INNER JOIN {customfield_field} cff ON cff.id = cfd.fieldid
+      WHERE ctxt.instanceid = c.id
+    ) = "Mooc"
+  ) /* aulas solo 100% virtual y blended */
+  AND rest.shortname = "student"
+  AND LOWER(survey.name) LIKE "%satisfacci%"
+
+  GROUP BY c.id, DATE_FORMAT(FROM_UNIXTIME(ueest.timeend), '%d/%m/%Y' )
+  ORDER BY c.id, fin DESC
+) aulas 
+GROUP BY Id_curso
