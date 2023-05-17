4fa798317a7a826711803281e7425b1772a0cdc5
Pablo Alberto Velasquez Rodriguez <PABLO.VELASQUEZ@uexternado.edu.co>
2023-05-17 14:19:17 -0500

    Reportes 2023-1

--- 20231/nuevos-informes-semestre-totales-inscripciones-y-estudiantes-2023-1.sql
+++ 20231/nuevos-informes-semestre-totales-inscripciones-y-estudiantes-2023-1.sql
@@ -1,5 +1,59 @@
 /* Nuevos informes semestre Totales inscripciones y estudiantes 2023-1 */
 SELECT "2023" AS "AÑO","I" AS "PERIODO/SEMESTRE",
+
+(
+    SELECT COUNT(DISTINCT c.id) AS "Aulas activas"
+    FROM mdl_course c
+    INNER JOIN mdl_context ctx ON ctx.instanceid = c.id
+    INNER JOIN mdl_role_assignments ra ON ctx.id = ra.contextid
+    INNER JOIN mdl_role r ON r.id = ra.roleid
+    INNER JOIN mdl_user u ON u.id = ra.userid
+    INNER JOIN mdl_course_categories cc ON c.category = cc.id
+    INNER JOIN mdl_enrol e ON e.courseid =c.id
+    INNER JOIN mdl_user_enrolments ue ON ue.userid = u.id AND ue.enrolid = e.id
+    WHERE
+
+    IF(  /* Condicional */
+        REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 2 /* PREGRADO */
+        AND
+        (
+            REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", '') = 5 /* Si es de la Facultad de Derecho */
+            OR
+            REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", '') = 1511 /* Si es del Instituto de Estudios Interdisciplinarios Richard Tovar Cárdenas */
+            OR
+            REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 4),LENGTH(SUBSTRING_INDEX(cc.path, "/", 4-1)) + 1),"/", '') = 968 /* o si son Examenes de Clasificación de la Facultad de Derecho */
+            OR
+            (
+                (
+                    REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", '') = 339 /* Si es del Departamento de Matemáticas */
+                    OR
+                    REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", '') = 374 /* Si es del Centro de Idiomas y Cultura */
+                )
+                AND
+                INSTR(LOWER(c.fullname),"derecho") != 0 /* y que dentro del nombre largo tengan la palabra "derecho" */
+            )
+        )
+        ,
+        ( /* aplican los filtros de fechas del calendario A y calendario B de Derecho */
+            (DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= @calAfechainicio AND DATE_FORMAT(FROM_UNIXTIME(ue.timeend), '%Y-%m-%d' ) <= @calAfechafin) /* Pregrado Calendario A */
+            OR 
+            (DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= @calBfechainicio AND DATE_FORMAT(FROM_UNIXTIME(ue.timeend), '%Y-%m-%d' ) <= @calBfechafin) /* Pregrado Calendario B */
+        )
+        , /* Sino, se aplica el filtro según la fecha normal semestral  */ 
+        (
+            DATE_FORMAT(FROM_UNIXTIME(ue.timestart), '%Y-%m-%d' ) >= @fechainiciosemestral
+            AND 
+            (
+                REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 2 /* PREGRADO */
+                OR REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 6 /* POSGRADO */
+                OR REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 2),LENGTH(SUBSTRING_INDEX(cc.path, "/", 2-1)) + 1),"/", '') = 11 /* EC */
+            )
+        )
+    )
+    AND r.shortname = "student"
+    ORDER BY c.id ASC
+) AS "AULAS ACTIVAS"
+,
 (
     SELECT COUNT(u.id) AS "Inscripciones Estudiantes"
     FROM mdl_course c
@@ -49,16 +103,12 @@ SELECT "2023" AS "AÑO","I" AS "PERIODO/SEMESTRE",
             )
         )
     )
-    AND LOCATE("Facultad",(SELECT cccc.name FROM mdl_course_categories cccc WHERE cccc.id
-    = (REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", '')))) != 0 /* Solo estudiantes de facultades */
-    AND DATE_FORMAT(FROM_UNIXTIME(ue.timecreated), '%Y-%m-%d' ) <= @fechafotoinformes /* Periodo 2023-1 */
     AND r.shortname = "student"
-
     ORDER BY c.id ASC
 ) AS "INSCRIPCIONES"
 ,
 (
-    SELECT COUNT(DISTINCT u.id) AS "Estudiantes Activos"
+    SELECT COUNT(distinct u.id) AS "Estudiantes activos"
     FROM mdl_course c
     INNER JOIN mdl_context ctx ON ctx.instanceid = c.id
     INNER JOIN mdl_role_assignments ra ON ctx.id = ra.contextid
@@ -106,11 +156,7 @@ SELECT "2023" AS "AÑO","I" AS "PERIODO/SEMESTRE",
             )
         )
     )
-    AND LOCATE("Facultad",(SELECT cccc.name FROM mdl_course_categories cccc WHERE cccc.id
-    = (REPLACE(SUBSTRING(SUBSTRING_INDEX(cc.path, "/", 3),LENGTH(SUBSTRING_INDEX(cc.path, "/", 3-1)) + 1),"/", '')))) != 0 /* Solo estudiantes de facultades */
-    AND DATE_FORMAT(FROM_UNIXTIME(ue.timecreated), '%Y-%m-%d' ) <= @fechafotoinformes /* Periodo 2023-1 */
     AND r.shortname = "student"
-
     ORDER BY c.id ASC
 ) AS "ESTUDIANTES"
 ,
